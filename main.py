8c29f5b765377ec36ce5c8fd3646451fa85ff4d6
klauss1977 <klauss1977@yahoo.com>
2023-11-10 16:23:07 +0200

    use list comprehension to build states_to_learn.csv

--- main.py
+++ main.py
@@ -9,28 +9,23 @@ turtle.shape(image)
 
 data = pandas.read_csv("50_states.csv")
 states_list = data.state.to_list()
-guessed_state = turtle.Turtle()
-guessed_state.penup()
-guessed_state.hideturtle()
-correct_answers = 0
+state_turtle = turtle.Turtle()
+state_turtle.penup()
+state_turtle.hideturtle()
 guessed_states_list = []
 while len(guessed_states_list) < 50:
-    answer_state = screen.textinput(title=f"{correct_answers}/50 States Correct",
+    answer_state = screen.textinput(title=f"{len(guessed_states_list)}/50 States Correct",
                                     prompt="What's another state name? ").title()
     if answer_state == "Exit":
+        states_to_learn = [state for state in states_list if state not in guessed_states_list]
+        pandas.DataFrame(states_to_learn).to_csv("states_to_learn.csv")
         break
     if answer_state in states_list:
-        correct_answers += 1
         guessed_states_list.append(answer_state)
         state_x = data[data.state == answer_state].x.values[0]
         state_y = data[data.state == answer_state].y.values[0]
-        guessed_state.goto(state_x, state_y)
-        guessed_state.write(f"{answer_state}")
+        state_turtle.goto(state_x, state_y)
+        state_turtle.write(f"{answer_state}")
 
 
-states_to_learn = {"State name":[]}
-for state in states_list:
-    if state not in guessed_states_list:
-        states_to_learn["State name"].append(state)
-print(states_to_learn)
-pandas.DataFrame(states_to_learn).to_csv("states_to_learn.csv")
+
