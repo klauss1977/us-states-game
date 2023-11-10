934369b991c66d849d657acc3e8248a21690058f
klauss1977 <klauss1977@yahoo.com>
2023-11-10 11:08:22 +0200

    final commit

--- main.py
+++ main.py
@@ -13,19 +13,24 @@ guessed_state = turtle.Turtle()
 guessed_state.penup()
 guessed_state.hideturtle()
 correct_answers = 0
-game_on = True
-while game_on:
+guessed_states_list = []
+while len(guessed_states_list) < 50:
     answer_state = screen.textinput(title=f"{correct_answers}/50 States Correct",
                                     prompt="What's another state name? ").title()
-    print(answer_state)
-    if answer_state:
-        if answer_state in states_list:
-            correct_answers += 1
-            state_x = data[data.state == answer_state].x.values[0]
-            state_y = data[data.state == answer_state].y.values[0]
-            guessed_state.goto(state_x, state_y)
-            guessed_state.write(f"{answer_state}")
-    else:
-        game_on = False
+    if answer_state == "Exit":
+        break
+    if answer_state in states_list:
+        correct_answers += 1
+        guessed_states_list.append(answer_state)
+        state_x = data[data.state == answer_state].x.values[0]
+        state_y = data[data.state == answer_state].y.values[0]
+        guessed_state.goto(state_x, state_y)
+        guessed_state.write(f"{answer_state}")
 
-screen.exitonclick()
+
+states_to_learn = {"State name":[]}
+for state in states_list:
+    if state not in guessed_states_list:
+        states_to_learn["State name"].append(state)
+print(states_to_learn)
+pandas.DataFrame(states_to_learn).to_csv("states_to_learn.csv")
